612b94af5263ab8215f08e213baadccf512391aa
sahilalam <alamsahil939@gmail.com>
2021-04-09 16:47:02 +0530

    4 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import React from "react";
+import { Button, Jumbotron, Navbar, Nav, Row, Col } from "react-bootstrap";
 import "./style.css";
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <Jumbotron className="m-0 p-5 jumbotron bg-light">
+        <Row className="justify-content-center p-5 mt-5 header">
+          <Col xs="12" md="10" className="text-center top-animation">
+            <h1 className="heading">Sahil Alam</h1>
+          </Col>
+          <Col xs="12" md="3" className="text-center bottom-animation">
+            <h5 className="text-animation mb-0 pb-0" />
+          </Col>
+        </Row>
+        <Navbar
+          collapseOnSelect
+          expand="lg"
+          bg="dark"
+          variant="dark"
+          className="nav-bar bottom-animation"
+        >
+          <Navbar.Brand href="/">Sahil Alam</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#skills">Skills</Nav.Link>
+              <Nav.Link href="#projects">Projects</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#about">About</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </Jumbotron>
     </div>
   );
 }
