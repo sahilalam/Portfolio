533a23ba74b8fd74cb98bd44af2d3f361b2ec9d2
sahilalam <alamsahil939@gmail.com>
2021-04-12 23:58:21 +0530

    2 files modified

--- src/Skills.js
+++ src/Skills.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { Row, Col, ProgressBar } from "react-bootstrap";
+import {
+  Row,
+  Col,
+  ProgressBar,
+  OverlayTrigger,
+  Tooltip
+} from "react-bootstrap";
 
 function Skills() {
   return (
@@ -14,109 +20,242 @@ function Skills() {
         <Col xs="12">
           <Row className="justify-content-start">
             <Col xs="12" md="6" className="mb-4 ">
-              <Col xs="12" className="header2 box-head mb-4 p-2 top-animation">
-                Front-End
-              </Col>
-              <Col xs="12" className="box-body mb-2 p-0 bottom-animation">
-                <Col xs="12" className="card p-3">
-                  <img
-                    className="logo-md p-2"
-                    src="https://raw.githubusercontent.com/sahilalam/SocialMediaProjectNodejs/master/src/public/Components/ICONS/html1.png"
-                  />
-                  HTML
-                  <ProgressBar variant="primary" now={80} className="bar" />
+              <Row className="justify-content-center">
+                <Col
+                  xs="12"
+                  className="header2 box-head mb-4 p-2 top-animation"
+                >
+                  Front-End
                 </Col>
-                <Col xs="12" className="card p-3">
-                  <img
-                    className="logo-md p-2"
-                    src="https://raw.githubusercontent.com/sahilalam/SocialMediaProjectNodejs/master/src/public/Components/ICONS/css.png"
-                  />
-                  CSS
-                  <ProgressBar variant="primary" now={80} className="bar" />
+                <Col xs="10" className="box-body mb-2 p-0 bottom-animation">
+                  <OverlayTrigger
+                    placement="right"
+                    overlay={<Tooltip>Working on HTML since Jan 2020</Tooltip>}
+                  >
+                    <Col xs="12" className="card p-3">
+                      <Row className="justify-content-center p-1">
+                        <Col xs="6" className="text-align-right">
+                          <img
+                            className="logo-md p-0"
+                            src="https://raw.githubusercontent.com/sahilalam/SocialMediaProjectNodejs/master/src/public/Components/ICONS/html1.png"
+                          />
+                        </Col>
+                        <Col xs="6" className="p-2">
+                          HTML
+                        </Col>
+                      </Row>
+                      <ProgressBar variant="primary" now={80} className="bar" />
+                    </Col>
+                  </OverlayTrigger>
+                  <OverlayTrigger
+                    placement="right"
+                    overlay={<Tooltip>Working on CSS since Jan 2020</Tooltip>}
+                  >
+                    <Col xs="12" className="card p-3">
+                      <Row className="justify-content-center p-1">
+                        <Col xs="6" className="text-align-right">
+                          <img
+                            className="logo-md p-2"
+                            src="https://raw.githubusercontent.com/sahilalam/SocialMediaProjectNodejs/master/src/public/Components/ICONS/css.png"
+                          />
+                        </Col>
+                        <Col xs="6" className="p-2">
+                          CSS
+                        </Col>
+                      </Row>
+                      <ProgressBar variant="primary" now={80} className="bar" />
+                    </Col>
+                  </OverlayTrigger>
+                  <OverlayTrigger
+                    placement="right"
+                    overlay={
+                      <Tooltip>Working on JavaScript since May 2020</Tooltip>
+                    }
+                  >
+                    <Col xs="12" className="card p-3">
+                      <Row className="justify-content-center p-1">
+                        <Col xs="6" className="text-align-right">
+                          <img
+                            className="logo-md p-2"
+                            src="https://raw.githubusercontent.com/sahilalam/SocialMediaProjectNodejs/master/src/public/Components/ICONS/js.png"
+                          />
+                        </Col>
+                        <Col xs="6" className="p-2">
+                          JAVASCRIPT
+                        </Col>
+                      </Row>
+                      <ProgressBar variant="primary" now={70} className="bar" />
+                    </Col>
+                  </OverlayTrigger>
+                  <OverlayTrigger
+                    placement="right"
+                    overlay={
+                      <Tooltip>Working on BootStrap since May 2020</Tooltip>
+                    }
+                  >
+                    <Col xs="12" className="card p-3">
+                      <Row className="justify-content-center p-1">
+                        <Col xs="6" className="text-align-right">
+                          <img
+                            className="logo-md p-2"
+                            src="https://raw.githubusercontent.com/sahilalam/SocialMediaProjectNodejs/master/src/public/Components/ICONS/Bootstrap.png"
+                          />
+                        </Col>
+                        <Col xs="6" className="p-2">
+                          BOOTSTRAP
+                        </Col>
+                      </Row>
+                      <ProgressBar variant="primary" now={80} className="bar" />
+                    </Col>
+                  </OverlayTrigger>
+                  <OverlayTrigger
+                    placement="right"
+                    overlay={
+                      <Tooltip>Working on React since March 2021</Tooltip>
+                    }
+                  >
+                    <Col xs="12" className="card p-3">
+                      <Row className="justify-content-center p-1">
+                        <Col xs="6" className="text-align-right">
+                          <img
+                            className="logo-md p-2"
+                            src="https://raw.githubusercontent.com/sahilalam/SocialMediaProjectNodejs/master/src/public/Components/ICONS/react.png"
+                          />
+                        </Col>
+                        <Col xs="6" className="p-2">
+                          REACT
+                        </Col>
+                      </Row>
+                      <ProgressBar variant="primary" now={80} className="bar" />
+                    </Col>
+                  </OverlayTrigger>
                 </Col>
-                <Col xs="12" className="card p-3">
-                  JAVASCRIPT
-                  <img
-                    className="logo-md p-2"
-                    src="https://raw.githubusercontent.com/sahilalam/SocialMediaProjectNodejs/master/src/public/Components/ICONS/js.png"
-                  />
-                  <ProgressBar variant="primary" now={70} className="bar" />
-                </Col>
-                <Col xs="12" className="card p-3">
-                  <img
-                    className="logo-md p-2"
-                    src="https://raw.githubusercontent.com/sahilalam/SocialMediaProjectNodejs/master/src/public/Components/ICONS/Bootstrap.png"
-                  />
-                  BOOTSTRAP
-                  <ProgressBar variant="primary" now={80} className="bar" />
-                </Col>
-                <Col xs="12" className="card p-3">
-                  <img
-                    className="logo-md p-2"
-                    src="https://raw.githubusercontent.com/sahilalam/SocialMediaProjectNodejs/master/src/public/Components/ICONS/react.png"
-                  />
-                  REACT
-                  <ProgressBar variant="primary" now={80} className="bar" />
-                </Col>
-              </Col>
+              </Row>
             </Col>
           </Row>
           <Row className="justify-content-center">
             <Col xs="12" md="6" className="mb-4 ">
-              <Col xs="12" className="header2 box-head mb-4 p-2">
-                Back-End
-              </Col>
-              <Col xs="12" className="box-body mb-2 p-0">
-                <Col xs="12" className="card p-3">
-                  <img
-                    className="logo-md p-2"
-                    src="https://raw.githubusercontent.com/sahilalam/SocialMediaProjectNodejs/master/src/public/Components/ICONS/Node.png"
-                  />
-                  Node.js
-                  <ProgressBar variant="primary" now={70} className="bar" />
+              <Row className="justify-content-center">
+                <Col xs="12" className="header2 box-head mb-4 p-2">
+                  Back-End
                 </Col>
-                <Col xs="12" className="card p-3">
-                  <img
-                    className="logo-md p-2"
-                    src="https://raw.githubusercontent.com/sahilalam/SocialMediaProjectNodejs/master/src/public/Components/ICONS/express.png"
-                  />
-                  Express.js
-                  <ProgressBar variant="primary" now={70} className="bar" />
+                <Col xs="10" className="box-body mb-2 p-0">
+                  <OverlayTrigger
+                    placement="right"
+                    overlay={
+                      <Tooltip>Working on Node.Js since May 2020</Tooltip>
+                    }
+                  >
+                    <Col xs="12" className="card p-3">
+                      <Row className="justify-content-center p-1">
+                        <Col xs="6" className="text-align-right">
+                          <img
+                            className="logo-md p-2"
+                            src="https://raw.githubusercontent.com/sahilalam/SocialMediaProjectNodejs/master/src/public/Components/ICONS/Node.png"
+                          />
+                        </Col>
+                        <Col xs="6" className="p-2">
+                          Node.js
+                        </Col>
+                      </Row>
+                      <ProgressBar variant="primary" now={70} className="bar" />
+                    </Col>
+                  </OverlayTrigger>
+                  <OverlayTrigger
+                    placement="right"
+                    overlay={
+                      <Tooltip>Working on Express.Js since May 2020</Tooltip>
+                    }
+                  >
+                    <Col xs="12" className="card p-3">
+                      <Row className="justify-content-center p-1">
+                        <Col xs="6" className="text-align-right">
+                          <img
+                            className="logo-md p-2"
+                            src="https://raw.githubusercontent.com/sahilalam/SocialMediaProjectNodejs/master/src/public/Components/ICONS/express.png"
+                          />
+                        </Col>
+                        <Col xs="6" className="p-2">
+                          Express.js
+                        </Col>
+                      </Row>
+                      <ProgressBar variant="primary" now={70} className="bar" />
+                    </Col>
+                  </OverlayTrigger>
+                  <OverlayTrigger
+                    placement="left"
+                    overlay={
+                      <Tooltip>Working on MongoDB since March 2021</Tooltip>
+                    }
+                  >
+                    <Col xs="12" className="card p-3">
+                      <Row className="justify-content-center p-1">
+                        <Col xs="6" className="text-align-right">
+                          <img
+                            className="logo-md p-2"
+                            src="https://raw.githubusercontent.com/sahilalam/SocialMediaProjectNodejs/master/src/public/Components/ICONS/mongodb.png"
+                          />
+                        </Col>
+                        <Col xs="6" className="p-2">
+                          MongoDB
+                        </Col>
+                      </Row>
+                      <ProgressBar variant="primary" now={70} className="bar" />
+                    </Col>
+                  </OverlayTrigger>
                 </Col>
-                <Col xs="12" className="card p-3">
-                  <img
-                    className="logo-md p-2"
-                    src="https://raw.githubusercontent.com/sahilalam/SocialMediaProjectNodejs/master/src/public/Components/ICONS/mongodb.png"
-                  />
-                  MongoDB
-                  <ProgressBar variant="primary" now={70} className="bar" />
-                </Col>
-              </Col>
+              </Row>
             </Col>
           </Row>
           <Row className="justify-content-end">
             <Col xs="12" md="6" className="mb-4 ">
-              <Col xs="12" className="header2 box-head mb-4 p-2">
-                Programming
-              </Col>
-              <Col xs="12" className="box-body mb-2 p-0">
-                <Col xs="12" className="card p-3">
-                  <img
-                    className="logo-md p-2"
-                    src="https://raw.githubusercontent.com/sahilalam/SocialMediaProjectNodejs/master/src/public/Components/ICONS/ds.png"
-                  />
-                  DS and Algo
-                  <ProgressBar variant="primary" now={80} className="bar" />
+              <Row className="justify-content-center">
+                <Col xs="12" className="header2 box-head mb-4 p-2">
+                  Programming
                 </Col>
-                <Col xs="12" className="card p-3">
-                  <img
-                    className="logo-md p-2"
-                    src="https://raw.githubusercontent.com/sahilalam/SocialMediaProjectNodejs/master/src/public/Components/ICONS/c++.png"
-                  />
-                  C++
-                  <ProgressBar variant="primary" now={70} className="bar" />
+                <Col xs="10" className="box-body mb-2 p-0">
+                  <OverlayTrigger
+                    placement="left"
+                    overlay={
+                      <Tooltip>Working on Ds and Algo since March 2020</Tooltip>
+                    }
+                  >
+                    <Col xs="12" className="card p-3">
+                      <Row className="justify-content-center p-1">
+                        <Col xs="6" className="text-align-right">
+                          <img
+                            className="logo-md p-2"
+                            src="https://raw.githubusercontent.com/sahilalam/SocialMediaProjectNodejs/master/src/public/Components/ICONS/ds.png"
+                          />
+                        </Col>
+                        <Col xs="6" className="p-2">
+                          DS and Algo
+                        </Col>
+                      </Row>
+                      <ProgressBar variant="primary" now={80} className="bar" />
+                    </Col>
+                  </OverlayTrigger>
+                  <OverlayTrigger
+                    placement="left"
+                    overlay={<Tooltip>Working on C++ since Jan 2020</Tooltip>}
+                  >
+                    <Col xs="12" className="card p-3">
+                      <Row className="justify-content-center p-1">
+                        <Col xs="6" className="text-align-right">
+                          <img
+                            className="logo-md p-2"
+                            src="https://raw.githubusercontent.com/sahilalam/SocialMediaProjectNodejs/master/src/public/Components/ICONS/c++.png"
+                          />
+                        </Col>
+                        <Col xs="6" className="p-2">
+                          C++
+                        </Col>
+                      </Row>
+                      <ProgressBar variant="primary" now={70} className="bar" />
+                    </Col>
+                  </OverlayTrigger>
                 </Col>
-              </Col>
+              </Row>
             </Col>
           </Row>
         </Col>
